98e701c37a1de8e899da202ff9db12acaf53f9cb
Zak Alfonse <zalfonse@ruelala.com>
2016-09-11 13:15:52 -0400

    Simple script to generate secret key for people

---
+++ init_project.py
@@ -0,0 +1,7 @@
+import random
+
+SECRET_KEY = ''.join([random.SystemRandom().choice('abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)') for i in range(50)])
+
+secret = file('env', 'w')
+secret.write('SECRET_KEY=', SECRET_KEY)
+secret.close()
