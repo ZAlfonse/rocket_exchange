3befac52f3222a706d3f8707aa5c5f85c95446de
= <alfonse.z@gmail.com>
2016-09-11 17:42:06 -0400

    Use python 3 open instead of file

--- init_project.py
+++ init_project.py
@@ -2,6 +2,6 @@ import random
 
 SECRET_KEY = ''.join([random.SystemRandom().choice('abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)') for i in range(50)])
 
-secret = file('env', 'w')
+secret = open('env', 'w')
 secret.write('SECRET_KEY=' + SECRET_KEY)
 secret.close()
