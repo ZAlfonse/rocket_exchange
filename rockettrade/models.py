54d8ff956f6b30aa7de8096cbd9db90d53e090ca
Zak Alfonse <zalfonse@ruelala.com>
2016-09-11 18:37:46 -0400

    Some ideas for the trade db model

--- rockettrade/models.py
+++ rockettrade/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class AuctionListing(models.Model):
+    # AuctionItems -- o2m auctionitem
+    # Seller -- fk to auth.user
+    # Buyer -- fk to auth.user
+    # Open Date -- datetime
+    # Closed Date -- datetime
+    # Status -- fk to AuctionStatus
+    pass
+
+
+class AuctionStatus(models.Model):
+    # name -- Completed, Open, Pending, Incomplete
+    # description
+    pass
+
+
+class AuctionItem(models.Model):
+    # item -- fk to rocketitems.item
+    # quantity -- number
+    # currency?
+    # price
+    pass
