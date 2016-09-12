b51416d454fe9bedece44eac78269606433b66f8
zalfonse <alfonse.z@gmail.com>
2016-09-11 21:36:59 -0400

    rockettrade models wip

--- rocket_exchange/settings.py
+++ rocket_exchange/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'rocketitems.apps.RocketitemsConfig',
+    'rockettrade.apps.RockettradeConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- rockettrade/models.py
+++ rockettrade/models.py
@@ -1,25 +1,40 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+from rocketitems.models import Item
 
 
-class AuctionListing(models.Model):
-    # AuctionItems -- o2m auctionitem
-    # Seller -- fk to auth.user
-    # Buyer -- fk to auth.user
-    # Open Date -- datetime
-    # Closed Date -- datetime
-    # Status -- fk to AuctionStatus
-    pass
+class Listing(models.Model):
+	OPEN = 'O'
+	PENDING = 'P'
+	CLOSED = 'C'
+	INCOMPLETE = 'I'
+
+	STATUS_CHOICES = (
+		(OPEN, 'Open'),
+		(PENDING, 'Pending'),
+		(CLOSED, 'Closed'),
+		(INCOMPLETE, 'Incomplete')
+	)
+
+	status = models.CharField(max_length=1, default=OPEN, choices=STATUS_CHOICES)
+	
+    item = models.ManyToManyField(Item, through='ListItem')
+    seller = models.ForeignKey(User)
+    
+    created = models.DateTimeField(auto_now_add=True)
+    edited = models.DateTimeField(auto_now=True)
+    closed = models.DateTimeField()
+    
+    def value(self):
+        self.item.quanity * self.item.price
+
+    def __str__(self):
+    	return '%s\'s offer of %i %s for %s' % self.seller.username, self.item.quantity, self.item.name, self.value
 
 
-class AuctionStatus(models.Model):
-    # name -- Completed, Open, Pending, Incomplete
-    # description
-    pass
-
-
-class AuctionItem(models.Model):
-    # item -- fk to rocketitems.item
-    # quantity -- number
-    # currency?
-    # price
-    pass
+class ListItem(models.Model):
+    item = models.ForeignKey(Item)
+    auction = models.ForeignKey(AuctionListing)
+    quantity = models.IntegerField(default=0)
+    price = models.DecimalField(max_digits=9, decimal_places=4)
