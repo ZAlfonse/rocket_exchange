3524e1d2bb47ee4e52f5f251e85374f2fca4a0eb
Zak Alfonse <zalfonse@ruelala.com>
2016-09-13 23:06:36 -0400

    Add steam dev key to env file

--- init_project.py
+++ init_project.py
@@ -1,7 +1,9 @@
 import random
 
 SECRET_KEY = ''.join([random.SystemRandom().choice('abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)') for i in range(50)])
+STEAM_KEY = input("We need a steam dev key (https://steamcommunity.com/dev/apikey): ")
 
 secret = open('env', 'w')
 secret.write('SECRET_KEY=' + SECRET_KEY)
+secret.write('\nSTEAM_KEY=' + STEAM_KEY)
 secret.close()
