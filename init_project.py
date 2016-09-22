86b0043d7fa9ab939b3914c18c552f5300d50792
Zak Alfonse <zalfonse@ruelala.com>
2016-09-22 11:47:50 -0400

    Composify

--- init_project.py
+++ init_project.py
@@ -4,7 +4,7 @@ SECRET_KEY = ''.join([random.SystemRandom().choice('abcdefghijklmnopqrstuvwxyz01
 STEAM_KEY = raw_input("We need a steam dev key (https://steamcommunity.com/dev/apikey): ")
 ADMIN_PASS = raw_input("And a password for 'admin' user: ")
 
-secret = open('env', 'w')
+secret = open('.env', 'w')
 secret.write('SECRET_KEY=' + SECRET_KEY)
 secret.write('\nSTEAM_KEY=' + STEAM_KEY)
 secret.write('\nADMIN_PASS=' + ADMIN_PASS)
